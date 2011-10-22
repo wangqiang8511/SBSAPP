91c2498ba77484a00bc21e38b6aa8cabeb2bea60
WUMUXIAN <wumuxian@WUMUXIANs-MacBook-Pro.local>
2011-10-22 19:47:02 +0800

    Initial Commit

---
+++ SBSAPP/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  SBSAPP
+//
+//  Created by WUMUXIAN on 22/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
